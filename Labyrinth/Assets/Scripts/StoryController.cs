124611f5428c833befdf64065259591468fd9806
ByrdC <ByrdC@users.noreply.github.com>
2020-09-04 18:58:54 -0400

    start/pause menu scene edits

--- Labyrinth/Assets/Scripts/StoryController.cs
+++ Labyrinth/Assets/Scripts/StoryController.cs
@@ -6,13 +6,14 @@ using TMPro;
 public class StoryController : MonoBehaviour
 {
     public TextMeshProUGUI StoryText;
+    public GameObject EndPanel;
 
     private string IntroText01 = "A red thread begins to unravel...";
     private string IntroText02 = "When Minos of Minoa was contesting his brothers for the throne, he asked the God of the Sea, Poseidon, to send a white bull as a symbol of his right to kingship.";
     private string IntroText03 = "Poseidon agreed, on the condition that Minos sacrifice the bull back to him, as thanks to Poseidon for the gift.";
     private string IntroText04 = "Minos took the bull and the throne, but out of greed did not sacrifice the gift. In anger, Poseidon cursed the wife of Minos, Pasiphae, to fall in love with the bull and mate with it.";
     private string IntroText05 = "The creature she gave birth to was the Minotaur which fed on human flesh and could not be controlled.";
-    private string IntroText06 = "Ashamed, but unwilling to kill the creature, Minos ordered the great inventor and architect Daedalus create a labyrinth which would hold the monster, a maze so complicated that no one could find the exit.";
+    private string IntroText06 = "Ashamed, but unwilling to kill the creature, Minos ordered the great inventor and architect Daedalus to create a labyrinth which would hold the monster, a maze so complicated that no one could find the exit.";
     private string IntroText07 = "When King Minos lost his son Androgeus to a fatal accident during the Panathenaic Games he was infuriated, and demanded Aegeus the king of Athens to send seven men and women every nine years to feed to the Minotaur.";
     private string IntroText08 = "Theseus, son of Aegeus, volunteered to be one of the seven young men that would go to Crete, in order to kill the Minotaur and end the human sacrifices to the monster.";
     private string IntroText09 = "When he arrived in Crete, he met the daughter of King Minos, Princess Ariadne. She saw potential in him and decided to give Theseus her weaving thread.";
@@ -88,6 +89,11 @@ public class StoryController : MonoBehaviour
         else if (collision.tag == "intro12")
         {
             StoryText.text = IntroText12;
+        }
+
+        else if (collision.tag == "ENDING")
+        {
+            EndPanel.SetActive(true);
         }
     }
 }
